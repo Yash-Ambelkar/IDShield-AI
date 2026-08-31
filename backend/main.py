0d4d14a86a5ac74cf61595284a522cf25c316029
Yash-Ambelkar <yashambelkar93@gmail.com>
2026-08-31 16:06:35 +0530

    Fix CORS for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -108,21 +108,17 @@ app.mount(
 # ==========================================================
 
 app.add_middleware(
-
     CORSMiddleware,
 
     allow_origins=[
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://id-shield-ai.vercel.app"
-],
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://id-shield-ai.vercel.app"
+    ],
 
     allow_credentials=True,
-
     allow_methods=["*"],
-
     allow_headers=["*"]
-
 )
 
 
